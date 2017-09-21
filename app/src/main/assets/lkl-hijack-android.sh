9ad07df58e1ed8e9f87ad32a2e78398c87824b3f
Hajime Tazaki <thehajime@gmail.com>
2017-09-22 08:45:58 +0900

    disable dhcp and use static ip addr for wifi
    due to unfamiliar dhcp server stall long time in a boot
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- app/src/main/assets/lkl-hijack-android.sh
+++ app/src/main/assets/lkl-hijack-android.sh
@@ -6,13 +6,18 @@ export PATH=${PATH}:${script_dir}
 
 # rmnet changes often, so just grep it
 RMNET_IP=$(ifconfig rmnet0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1)
-RMNET_GATEWAY=$(ip route | grep "via" | cut -d " " -f 3 | head -n 1)
 RMNET_MASK=$(ip route | grep rmnet0 | grep '/' | head -n 1 | cut -d " " -f 1 | cut -d "/" -f 2)
+RMNET_GATEWAY=$(ip route | grep "via" | grep rmnet0 | cut -d " " -f 3 | head -n 1)
 
 # wifi configs
 WIFI_MAC=$(ip ad show dev wlan0 | grep link/ether | cut -d " " -f6)
+# rmnet changes often, so just grep it
+WIFI_IP=$(ifconfig wlan0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1)
+WIFI_MASK=$(ip route | grep wlan0 | grep '/' | head -n 1 | cut -d " " -f 1 | cut -d "/" -f 2)
+WIFI_GATEWAY=$(ip route show table all | grep "via" | grep wlan0 | cut -d " " -f 3 | head -n 1)
 
 #LKL_HIJACK_DEBUG=1
+# LKL_HIJACK_BOOT_CMDLINE="ip=dhcp"
 
 export LKL_HIJACK_SYSCTL="net.ipv4.tcp_rmem=524288 1048576 2097152;net.ipv4.tcp_wmem=262144 524288 1048576"
 export LKL_HIJACK_SINGLE_CPU=2
@@ -22,8 +27,9 @@ iptables -A OUTPUT -p tcp --tcp-flags RST RST  -j DROP
 
 LD_PRELOAD=${script_dir}/${LKL_HIJACK_LIBNAME}   \
  LKL_HIJACK_NET_IFTYPE0=raw LKL_HIJACK_NET_IFPARAMS0=wlan0 \
+ LKL_HIJACK_NET_IP0=$WIFI_IP LKL_HIJACK_NET_NETMASK_LEN0=$WIFI_MASK LKL_HIJACK_NET_GATEWAY0=$WIFI_GATEWAY \
+ LKL_HIJACK_NET_GATEWAY=$WIFI_GATEWAY \
  LKL_HIJACK_NET_MAC0=$WIFI_MAC \
- LKL_HIJACK_BOOT_CMDLINE="ip=dhcp" \
  LKL_HIJACK_NET_IFTYPE1=raw-ipenc LKL_HIJACK_NET_IFPARAMS1=rmnet0 \
  LKL_HIJACK_NET_IP1=$RMNET_IP LKL_HIJACK_NET_NETMASK_LEN1=$RMNET_MASK LKL_HIJACK_NET_GATEWAY1=$RMNET_GATEWAY \
  $*
