74878d225e3485a196b9c2c24fc5674353a0d602
Hajime Tazaki <thehajime@gmail.com>
2017-10-27 17:47:25 +0900

    update syntax of the latest lkl
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- app/src/main/assets/lkl-hijack-android.sh
+++ app/src/main/assets/lkl-hijack-android.sh
@@ -16,9 +16,6 @@ WIFI_IP=$(ifconfig wlan0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1)
 WIFI_MASK=$(ip route | grep wlan0 | grep '/' | head -n 1 | cut -d " " -f 1 | cut -d "/" -f 2)
 WIFI_GATEWAY=$(ip route show table all | grep "via" | grep wlan0 | head -n 1 | cut -d " " -f 3)
 
-#LKL_HIJACK_DEBUG=1
-# LKL_HIJACK_BOOT_CMDLINE="ip=dhcp"
-
 DEF_GATEWAY=$WIFI_GATEWAY
 if [ -z $WIFI_GATEWAY ] ; then
     DEF_GATEWAY=$RMNET_GATEWAY
@@ -29,19 +26,40 @@ then
   trap '' TSTP
 fi
 
-export LKL_HIJACK_SYSCTL="net.ipv4.tcp_rmem=524288 1048576 2097152;net.ipv4.tcp_wmem=262144 524288 1048576"
-export LKL_HIJACK_SINGLE_CPU=2
 
 # Tweaks...
 iptables -A OUTPUT -p tcp --tcp-flags RST RST  -j DROP
 
-LD_PRELOAD=${script_dir}/${LKL_HIJACK_LIBNAME}   \
- LKL_HIJACK_NET_IFTYPE0=raw LKL_HIJACK_NET_IFPARAMS0=wlan0 \
- LKL_HIJACK_NET_IP0=$WIFI_IP LKL_HIJACK_NET_NETMASK_LEN0=$WIFI_MASK LKL_HIJACK_NET_GATEWAY0=$WIFI_GATEWAY \
- LKL_HIJACK_NET_GATEWAY=$DEF_GATEWAY \
- LKL_HIJACK_NET_MAC0=$WIFI_MAC \
- LKL_HIJACK_NET_IFTYPE1=raw-ipenc LKL_HIJACK_NET_IFPARAMS1=rmnet0 \
- LKL_HIJACK_NET_IP1=$RMNET_IP LKL_HIJACK_NET_NETMASK_LEN1=$RMNET_MASK LKL_HIJACK_NET_GATEWAY1=$RMNET_GATEWAY \
- $*
+
+# configs
+#export LKL_HIJACK_SYSCTL="net.ipv4.tcp_rmem=524288 1048576 2097152;net.ipv4.tcp_wmem=262144 524288 1048576"
+#export LKL_HIJACK_SINGLE_CPU=2
+export LKL_HIJACK_CONFIG_FILE="${script_dir}/lkl-hijack.json"
+
+cat <<EOF > ${script_dir}/lkl-hijack.json
+{
+    "gateway": "$DEF_GATEWAY",
+    "interfaces": [
+        {
+            "ifgateway": "$WIFI_GATEWAY",
+            "ip": "$WIFI_IP",
+            "mac": "$WIFI_MAC",
+            "masklen": "$WIFI_MASK",
+            "param": "wlan0",
+            "type": "raw"
+        },
+        {
+            "ifgateway": "$RMNET_GATEWAY",
+            "ip": "$RMNET_IP",
+            "masklen": "$RMNET_MASK",
+            "param": "rmnet0",
+            "type": "raw-ipenc"
+        }
+    ],
+    "singlecpu": "1"
+}
+EOF
+
+LD_PRELOAD=${script_dir}/${LKL_HIJACK_LIBNAME} $*
 
 iptables -D OUTPUT -p tcp --tcp-flags RST RST  -j DROP
